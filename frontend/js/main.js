cd839cb3acd82dd2fe1a357e7e3b9766b2502a9e
angelarc <230134@utags.edu.mx>
2026-07-27 13:49:34 -0600

    Se incorporó la lógica inicial del frontend.

---
+++ frontend/js/main.js
@@ -0,0 +1,9 @@
+// Funciones iniciales de la plataforma Bookia
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Bookia iniciada correctamente.");
+});
+
+function mostrarMensaje() {
+    console.log("Bienvenido a Bookia");
+}
